1b023aeea0adcd22613427375a8fda9ac64f9dfd
Caglar Kilcioglu <ckilcioglu@gmail.com>
2020-01-12 23:23:21 +0300

    application blog was added

---
+++ blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
